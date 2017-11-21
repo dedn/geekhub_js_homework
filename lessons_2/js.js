383075f57be688402fabe5fe3c14b4b28d6ef0b5
dedn <yura.kirillov@mev.com>
2017-11-21 11:01:24 +0200

    added first lessons

---
+++ lessons_2/js.js
@@ -0,0 +1,13 @@
+/**
+ * Created by user on 14.11.17.
+ */
+
+var input = $('#text')
+var submit = $('#submit')
+console.log('test')
+
+input.prop('value', 'test')
+submit.click(function () {
+  console.log('test')
+
+});
