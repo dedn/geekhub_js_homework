e597ffe64b386cd17fb9fcc712c18d329a97ca4d
dedn <yura.kirillov@mev.com>
2017-12-06 15:57:29 +0200

    update gitignore

--- lesson3/controllers/create-user.js
+++ lesson3/controllers/create-user.js
@@ -2,23 +2,18 @@ var path = require('path')
 
 module.exports =  function (request, response) {
 
-  if(request.body.type !== '3'){
-    response.sendFile(path.resolve(__dirname +'/../public/error.html'))
-  } else {
-    response.sendFile(path.resolve(__dirname +'/../public/sucses.html'))
+    var user = request.body;
 
-  var user = request.body;
+    if (user.type === '3') {
+      // save user in db
+      response.json({
+        id: 1,
+        phone: '38' + user.phone
+      })
+    } else {
+      response.status(500).json({
+        message: 'type is not valid'
+      })
 
-  if(user.type === '3'){
-    // save user in db
-    response.json({
-      id:1,
-      phone:'38' + user.phone
-    })
-  } else {
-    response.status(500).json({
-      message:'type is not valid'
-    })
-
+    }
   }
-}
--- lesson3/public/js/js.js
+++ lesson3/public/js/js.js
@@ -1,42 +1,45 @@
 (function () {
-<<<<<<< HEAD
-    var createUserForm = JQuery('#create-frined')
+
+    var createUserForm = JQuery('#create-user')
 
     createUserForm.on('submit', function (e) {
         event.preventDefault();
 
         JQuery.ajax({
             method:'POST',
+            url:'/create-user',
             data:createUserForm.serialize(),
             success:function (result) {
-                
+                result
             },
 
-            error:
+            error:function (error) {
+                error
+            }
         })
     })
 })
-=======
 
-  var createUserForm = JQuery('#create-user');
 
-  createUserForm.on('submit', function (event) {
-    event.preventDefault()
+  // var createUserForm = JQuery('#create-user');
+  //
+  // createUserForm.on('submit', function (event) {
+  //   event.preventDefault()
+  //
+  //   JQuery.ajax({
+  //     method:'POST',
+  //     url:'create-user',
+  //     date: createUserForm.serialize(),
+  //
+  //     success: function (result) {
+  //       result;
+  //     },
+  //     error: function (error) {
+  //       error;
+  //     }
+  //
+  //   })
+  //
+  // })
+// })
 
-    JQuery.ajax({
-      method:'POST',
-      url:'create-user',
-      date: createUserForm.serialize(),
-
-      success: function (result) {
-        result;
-      },
-      error: function (error) {
-        error;
-      }
-
-    })
-
-  })
-})
->>>>>>> 21d17e95506eaf9da9bf351871bb546529fc611f
